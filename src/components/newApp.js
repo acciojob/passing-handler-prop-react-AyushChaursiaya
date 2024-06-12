99d7082a3b36d2c75859b9401fa0d201297e0dba
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-06-12 14:14:35 +0000

    updated

---
+++ src/components/newApp.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import './newApp.css';
+
+const Handler = () => {
+    const [selectedColor, setSelectedColor] = useState('');
+
+    const handleColorChange = (color) => {
+        console.log(color);
+        setSelectedColor(color);
+    }
+
+    const handleBoxChange = (boxId) => {
+        document.getElementById(boxId).style.backgroundColor = selectedColor;
+    }
+    return (<div className="app">
+        <h2>'Select the gradient and then the Box to change the color'</h2>
+        <div className="buttons">
+            <button className="btn blue" onClick={() => handleColorChange('blue')}>Blue</button>
+            <button className="btn orange" onClick={() => handleColorChange('orange')}>Orange</button>
+            <button className="btn green" onClick={() => handleColorChange('green')}>Green</button>
+        </div>
+        <div className="boxes">
+            <div className="box" id="box1" onClick={() => handleBoxChange('box1')}>Selection</div>
+            <div className="box" id="box2" onClick={() => handleBoxChange('box2')}>Selection</div>
+            <div className="box" id="box3" onClick={() => handleBoxChange('box3')}>Selection</div>
+        </div>
+    </div>)
+}
+
+export default Handler;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Handler from "./components/newApp";
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Handler />, document.getElementById("root"));
